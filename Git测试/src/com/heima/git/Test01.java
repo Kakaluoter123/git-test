fd2370cf17cb4056defb439550cb6f084eacfc32
Kakaluoter123 <1822328066@qq.com>
2020-12-18 15:41:49 +0800

    把本地工程添加到git

---
+++ Git测试/src/com/heima/git/Test01.java
@@ -0,0 +1,13 @@
+package com.heima.git;
+
+public class Test01 {
+     public static void main(String[] args) {
+          System.out.println("hello git project");
+          System.out.println("hello git project");
+          System.out.println("hello git project");
+          System.out.println("hello git project");
+          System.out.println("hello git project");
+          System.out.println("hello git project");
+          System.out.println("hello git project");
+     }
+}
